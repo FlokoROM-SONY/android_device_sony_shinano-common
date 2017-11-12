749a00d93879f8428862b781c29bdadecf735d46
Kousuke Nagatsuka <koron393@gmail.com>
2017-11-12 23:01:42 +0900

    shinano-common: TWRP Multirom
    Change-Id: I762eda820d9dd47e2c68c04fbd86e30ea3e91c86

---
+++ multirom/mr_qcom_overlay.h
@@ -0,0 +1,7 @@
+#ifndef MR_QCOM_OVERLAY_H
+#define MR_QCOM_OVERLAY_H
+
+#include "../../../../kernel/sony/msm8974/include/linux/msm_ion.h"
+#include "../../../../kernel/sony/msm8974/include/linux/msm_mdp.h"
+
+#endif

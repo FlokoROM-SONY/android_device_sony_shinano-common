710296822d8e44b09ce10c6a0941b09885e56b80
koron393 <koron393@gmail.com>
2018-05-03 23:01:06 +0900

    shinano-common: recovery: Fix for Lineage build
    Change-Id: I6908122cbfe3f4e079be4d19ab64952ab652369d

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -11,19 +11,19 @@ LOCAL_POST_INSTALL_CMD += \
     cp -uv $(TARGET_ROOT_OUT)/init.real $(TARGET_RECOVERY_ROOT_OUT)/init ;
 
 LOCAL_ADDITIONAL_DEPENDENCIES += \
-    twrpdec \
     $(TARGET_ROOT_OUT)/init \
     $(TARGET_ROOT_OUT)/init.real \
     init_sony
+#    twrpdec \
 
 # if hw keystore, build keystore support and create /system symlinks
-ifeq ($(TARGET_HW_KEYSTORE), true)
-#we have hw keystore, add to build
-LOCAL_ADDITIONAL_DEPENDENCIES += \
-    libvolddecrypt \
-    init.recovery.vold_decrypt.rc
-
-endif
+#ifeq ($(TARGET_HW_KEYSTORE), true)
+##we have hw keystore, add to build
+#LOCAL_ADDITIONAL_DEPENDENCIES += \
+#    libvolddecrypt \
+#    init.recovery.vold_decrypt.rc
+#
+#endif
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_PREBUILT)
 

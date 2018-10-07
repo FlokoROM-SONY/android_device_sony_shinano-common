488c47eae68b057f9fdc67585169967f1672eca8
Bruno Martins <bgcngm@gmail.com>
2018-10-08 00:25:18 +0100

    shinano-common: Create /persist folder outside of vendor init script
     * The core SELinux policies for vendor_init are being restricted as to
       comply with Treble. Simply adding a local rule to allow creating
       folders under rootfs would be enough, but at the same time defining
       the extra root folders and making use of AID/fs configuration file
       to set perms is more neat and clean.
    
     * Also create idd lta-label tmp folder for shinano device.
    
    Change-Id: I01096979dc0ce9b8588af477855af91b77c89019
    Signed-off-by: koron393 <koron393@gmail.com>

---
+++ config.fs
@@ -0,0 +1,23 @@
+[idd/]
+mode: 0751
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[lta-label/]
+mode: 0555
+user: AID_ROOT
+group: AID_ROOT
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[tmp/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0

f4cbb07134452c1e3ea84ab61ad31d9546d2779e
koron393 <koron393@gmail.com>
2018-11-25 00:39:51 +0900

    shinano-common: Add PRODUCT_PLATFORM_SOD
    * Need for macaddrsetup build barrier

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+PRODUCT_PLATFORM_SOD := true
+
 # inherit from msm8974-common
 include device/sony/msm8974-common/BoardConfigCommon.mk
 
